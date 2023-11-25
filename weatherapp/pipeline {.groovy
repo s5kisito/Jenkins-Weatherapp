2bbdc54d7359c5d4c20199398df4d69099975d41
devopseasylearning <info@devopseasylearning.com>
2023-11-24 23:45:16 -0500

    building

--- weatherapp/pipeline {.groovy
+++ weatherapp/pipeline {.groovy
@@ -1,20 +1,3 @@
 
-pipeline {
-    agent any
 
-    stages {
-        stage('Login to Docker Hub') {
-            steps {
-                script {
-                    // Define your Docker Hub credentials
-                    def dockerHubUser = 'christ.nyc@gmail.com'
-                    def dockerHubPassword = 'chriseven'
-
-                    // Docker login command
-                    sh "docker login -u ${dockerHubUser} -p ${dockerHubPassword}"
-                }
-            }
-        }
-    }
-}
 
